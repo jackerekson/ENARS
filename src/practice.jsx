b832a27a7ae80ba83b6b04727eecbcd4b5ac25e0
jackerekson <erekson.jack@gmail.com>
2022-05-31 21:36:18 -0500

    adding local changes

---
+++ src/practice.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./practice.css"
+
+function Practice() {
+
+
+  return (
+    <div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Practice;
